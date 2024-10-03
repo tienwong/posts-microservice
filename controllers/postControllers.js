abd7fb4f7a8cf5a90f9211a50a058ee5933676c7
Tien <45297291+tienwong@users.noreply.github.com>
2024-10-02 20:49:42 -0500

    feat: createNewPost validates that user is authorized

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -5,6 +5,9 @@ const getPostsByUsername = async (username) => {
 }
 
 const createNewPost = async (username, text) => {
+    if (!username) {
+        return 401
+    }
 }
 
 const editPost = async (postUuid, text) => {
--- controllers/postControllers.test.js
+++ controllers/postControllers.test.js
@@ -1,2 +1,8 @@
 const { createNewPost, deletePost, editPost, getPostsByUsername } = require('./postControllers')
 
+describe('createNewPost', () => {
+    it('Should return status code 401 (unauthorized) when username is not provided', async () => {
+        const res = await createNewPost()
+        expect(res).toBe(401)
+    })
+})

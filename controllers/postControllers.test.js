26b9392695ce21ffc7e9765c61977df0351c8f57
Tien <45297291+tienwong@users.noreply.github.com>
2024-10-02 21:59:57 -0500

    refactor: Rename variable for readability

--- controllers/postControllers.test.js
+++ controllers/postControllers.test.js
@@ -5,20 +5,20 @@ jest.mock('../models/services/postService')
 
 describe('createNewPost', () => {
     it('Should return status code 401 (unauthorized) when username is not provided', async () => {
-        const res = await createNewPost()
-        expect(res).toBe(401)
+        const result = await createNewPost()
+        expect(result).toBe(401)
     })
     it('Should return status 400 (bad input) when text is not provided', async () => {
-        const res = await createNewPost('bartsimpson99')
-        expect(res).toBe(400)
+        const result = await createNewPost('bartsimpson99')
+        expect(result).toBe(400)
     })
     it('Should return status 400 when username is not a string', async () => {
-        const res = await createNewPost({}, 'i just took a shower')
-        expect(res).toBe(400)
+        const result = await createNewPost({}, 'i just took a shower')
+        expect(result).toBe(400)
     })
     it('Should return status 400 when text is not a string', async () => {
-        const res = await createNewPost('notnedflanders', {})
-        expect(res).toBe(400)
+        const result = await createNewPost('notnedflanders', {})
+        expect(result).toBe(400)
     })
     it('Should create a new post and try to write it to the database when valid username and text are provided', async () => {
         const databaseLogic = jest.spyOn(postService, 'addPostToDatabase')
@@ -26,13 +26,13 @@ describe('createNewPost', () => {
         expect(databaseLogic).toHaveBeenCalled()
     })
     it('Should return status code 200 (success) when the database write succeeds', async () => {
-        postService.addPostToDatabase.mockResolvedValueOnce({ msg: 'success' })
-        const res = await createNewPost('fanfiction_ned_flanders', 'well howdy, postverse!')
-        expect(res).toBe(200)
+        postService.addPostToDatabase.mockresultolvedValueOnce({ msg: 'success' })
+        const result = await createNewPost('fanfiction_ned_flanders', 'well howdy, postverse!')
+        expect(result).toBe(200)
     })
     it('Should return 500 (internal server error) when the database write fails', async () => {
         postService.addPostToDatabase.mockRejectedValueOnce({ error: 'Some error'})
-        const res = await createNewPost('nothomersimpson', 'd\'oh!')
-        expect(res).toBe(500)
+        const result = await createNewPost('nothomersimpson', 'd\'oh!')
+        expect(result).toBe(500)
     })
 })

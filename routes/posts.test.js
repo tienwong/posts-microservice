258f5bb49b5069ecb50c4bee85e24c254dcdfebb
Tien <45297291+tienwong@users.noreply.github.com>
2024-10-05 14:31:35 -0500

    feat: POST /posts/:username/newPost returns 200 and a success message if the request body is valid and the post was successfully written to the database

---
+++ routes/posts.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+const app = require('../app')
+const postService = require('../models/services/postService')
+
+jest.mock('../models/services/postService')
+
+describe('POST /posts/:username/newPost', () => {
+    it('should return 200 and a success message if the request body is valid and the post was successfully written to the database', async () => {
+        postService.addPostToDatabase.mockResolvedValueOnce({ msg: 'success' })
+        const res = await request(app).post('/posts/bartsimpson/newPost').send({
+            text: 'eat my shorts'
+        })
+        console.log(res)
+        expect(res.statusCode).toEqual(200)
+        expect(res._body.message).toEqual('Post successfully added.')
+    })
+})
